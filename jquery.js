ac511338772d3595d4b5d90627d71f81d40cd429
guillaumepoucet <poucet@simplon-charleville.fr>
2020-05-05 12:44:02 +0200

    placeholder ok

--- jquery.js
+++ jquery.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    var placeholder = $('input').attr("placeholder");
+
     $('input').focus(function() {
         $('.form-group').addClass("floating-label-form-group-with-value label");
-        $('input').attr("placeholder", "");
+        $('input').addClass("input-focus");
     })
     $('input').focusout(function() {
         $('.form-group').removeClass("floating-label-form-group-with-value label");
-        $('input').attr("placeholder", placeholder);
+        $('input').removeClass("input-focus");
     })
 })
